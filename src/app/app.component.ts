afb6ec81e12797be972504c358c225230efa0bfc
hrmn-1 <w0858805@myscc.ca>
2024-09-20 19:02:33 -0400

    Completed Assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from  '@angular/common';
+import { smartPhone } from '../shared/models/user';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Assignment 1';
-  myVariable1: string = 'harmanpreet kaur';
-  myVariable2: number = 32;
+  title = 'User Generation';
+
+  smartPhonesList: smartPhone[] = [
+    {model: "iphone14", color: "Black", size: 6, price: 600, isWaterproof: true},
+    {model: "iphone15pro", color: "Silver", size: 5, price: 800, isWaterproof: true},
+    {model: "Samsung", color: "Blue", size: 7, price: 700, isWaterproof: false},
+    {model: "iphone16", color: "Pink", size: 9, price: 1400, isWaterproof: true},
+    {model: "Vivo", color: "Red", size: 6, price: 500, isWaterproof: false},
+    {model: "iphone13", color: "Yellow", size: 7, price: 498, isWaterproof: true}
+  ];
+
 }
-
-
