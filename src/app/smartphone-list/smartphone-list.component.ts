8a4dd328e72e9211f5a3dde95b80095d6d5db808
hrmn-1 <w0858805@myscc.ca>
2024-09-26 21:07:52 -0400

    completed Assignmemt 3

--- src/app/smartphone-list/smartphone-list.component.ts
+++ src/app/smartphone-list/smartphone-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForOf } from "@angular/common";
+import {NgClass, NgForOf} from "@angular/common";
 import { SmartphoneListItemComponent } from "../smartphone-list-item/smartphone-list-item.component";
 
 
@@ -8,7 +8,8 @@ import { SmartphoneListItemComponent } from "../smartphone-list-item/smartphone-
   standalone: true,
   imports: [
     NgForOf,
-    SmartphoneListItemComponent
+    SmartphoneListItemComponent,
+    NgClass
   ],
   templateUrl: './smartphone-list.component.html',
   styleUrls: ['./smartphone-list.component.css']
@@ -17,13 +18,13 @@ export class SmartphoneListComponent {
 
   // Define smartphoneItems array using the smartPhone interface
   smartphoneItems = [
-    { model: 'iPhone 14', color: 'Black', size: 6.1, price: 999, isWaterproof: true },
-    { model: 'Galaxy S23', color: 'Phantom Black', size: 6.8, price: 1199, isWaterproof: true },
-    { model: 'Pixel 7', color: 'Snow', size: 6.3, price: 899, isWaterproof: true },
-    { model: 'OnePlus 11', color: 'Titan Black', size: 6.7, price: 799, isWaterproof: true }
+    { model: 'iPhone 13', color: 'Black', size: 6.1, price: 999, isWaterproof: true,image: 'src/image/iphone13.jpeg' },
+    { model: 'Galaxy S23', color: 'Phantom Black', size: 6.8, price: 1199, isWaterproof: true,image: 'src/image/samsung galaxy s21.jpeg' },
+    { model: 'Pixel 6', color: 'Snow', size: 6.3, price: 899, isWaterproof: true,image: 'src/image/google pixel 6.jpeg' },
+    { model: 'OnePlus 9', color: 'Titan Black', size: 6.7, price: 799, isWaterproof: true,image: 'src/image/oneplus9.jpeg' }
   ];
 
-  selectedSmartphoneItem: any;  // Updated to use the smartPhone interface
+  selectedSmartphoneItem: any;
 
   // Method to select a smartphone
   selectSmartphoneItem(item: any) {
