d89e5d40cd61e6b38ac5947d8ddf9e2cba60d40b
hrmn-1 <w0858805@myscc.ca>
2024-10-04 19:33:33 -0400

    create services folder and move IConent array out of contentlist

---
+++ src/shared/mockSmartphone.data.ts
@@ -0,0 +1,13 @@
+//mockStudent.data.ts
+//import the interface
+
+import { smartPhone } from './models/smartphone';
+
+
+export const smartPhoneList: smartPhone[] = [
+  { model: "iPhone 16", color: "Black", size: 6.1, price: 799, isWaterproof: true },
+  { model: "Samsung Galaxy F22", color: "White", size: 6.2, price: 699, isWaterproof: true },
+  { model: "Google Pixel 9", color: "Sorta Sage", size: 6.4, price: 599, isWaterproof: true },
+  { model: "Vivo", color: "Astral Black", size: 6.55, price: 729, isWaterproof: false }
+  // Add more as needed
+];
