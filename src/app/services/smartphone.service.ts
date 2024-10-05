1ceef0ef2ccede13a05ca8548e9aafe5bfb850ec
hrmn-1 <w0858805@myscc.ca>
2024-10-04 20:13:30 -0400

    created new observable method and create new service

--- src/app/services/smartphone.service.ts
+++ src/app/services/smartphone.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {smartPhoneList} from "../../shared/mockSmartphone.data";
+import { Observable, of} from "rxjs";
+import {smartPhone} from "../../shared/models/smartphone";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SmartphoneService {
+  private smartphones: smartPhone[] = smartPhoneList; // Local copy of smartphone data for CRUD Operations
 
   constructor() { }
+  // Return all smartphones
+  getSmartphones(): Observable<smartPhone[]> {
+    return of(smartPhoneList);
+  }
 }
--- src/app/smartphone-list/smartphone-list.component.ts
+++ src/app/smartphone-list/smartphone-list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import {NgClass, NgForOf} from "@angular/common";
-import { SmartphoneListItemComponent } from "../smartphone-list-item/smartphone-list-item.component";
+import {Component, OnInit} from '@angular/core';
+import {CommonModule, NgForOf} from "@angular/common";
+import {SmartphoneListItemComponent } from "../smartphone-list-item/smartphone-list-item.component";
+import {smartPhone} from "../../shared/models/smartphone";
+import {SmartphoneService} from "../services/smartphone.service";
 
 
 @Component({
@@ -9,26 +11,24 @@ import { SmartphoneListItemComponent } from "../smartphone-list-item/smartphone-
   imports: [
     NgForOf,
     SmartphoneListItemComponent,
-    NgClass
+    CommonModule
   ],
   templateUrl: './smartphone-list.component.html',
   styleUrls: ['./smartphone-list.component.css']
 })
-export class SmartphoneListComponent {
+export class SmartphoneListComponent implements OnInit {
 
-  // Define smartphoneItems array using the smartPhone interface
-  smartphoneItems = [
-    { model: 'iPhone 13', color: 'Black', size: 6.1, price: 999, isWaterproof: true,image: 'src/image/iphone13.jpeg' },
-    { model: 'Galaxy S23', color: 'Phantom Black', size: 6.8, price: 1199, isWaterproof: true,image: 'src/image/samsung galaxy s21.jpeg' },
-    { model: 'Pixel 6', color: 'Snow', size: 6.3, price: 899, isWaterproof: true,image: 'src/image/google pixel 6.jpeg' },
-    { model: 'OnePlus 9', color: 'Titan Black', size: 6.7, price: 799, isWaterproof: true,image: 'src/image/oneplus9.jpeg' }
-  ];
+  smartPhoneList: smartPhone[] = [];
 
-  selectedSmartphoneItem: any;
-
-  // Method to select a smartphone
-  selectSmartphoneItem(item: any) {
-    this.selectedSmartphoneItem = item;
+  constructor(private smartphoneService: SmartphoneService) {
   }
 
+  ngOnInit() {
+    //This lifecycle hook is a good place to fetch and init our data
+    this.smartphoneService.getSmartphones().subscribe({
+      next: (data: smartPhone[]) => this.smartPhoneList = data,
+      error: err => console.error("Error fetching Smartphone", err),
+      complete: () => console.log("Smartphone data fetch complete!")
+    })
+  }
 }
