ff44696e9285e25490894e2f88530fb391dfb68f
hrmn-1 <w0858805@myscc.ca>
2024-10-04 22:08:39 -0400

    update content list and done with observable pattern

--- src/app/services/smartphone.service.ts
+++ src/app/services/smartphone.service.ts
@@ -14,4 +14,23 @@ export class SmartphoneService {
   getSmartphones(): Observable<smartPhone[]> {
     return of(smartPhoneList);
   }
+  getSmartphoneByModel(model: string): Observable<smartPhone | undefined> {
+    const smartphone = this.smartphones.find(smartphone => smartphone.model === model);
+    return of(smartphone);
+  }
+  addSmartphone(newSmartphone: smartPhone): Observable<smartPhone[]> {
+    this.smartphones.push(newSmartphone);
+    return of(this.smartphones);
+  }
+  updateSmartphone(updatedSmartphone: smartPhone): Observable<smartPhone[]> {
+    const index = this.smartphones.findIndex(phone => phone.model === updatedSmartphone.model);
+    if (index !== -1) {
+      this.smartphones[index] = updatedSmartphone;
+    }
+    return of(this.smartphones);
+  }
+  deleteSmartphone(model: string): Observable<smartPhone[]> {
+    this.smartphones = this.smartphones.filter(phone => phone.model !== model);
+    return of(this.smartphones);
+  }
 }
--- src/app/smartphone-list-item/smartphone-list-item.component.ts
+++ src/app/smartphone-list-item/smartphone-list-item.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
-import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {Component, Input, OnInit} from '@angular/core';
+import {smartPhone} from "../../shared/models/smartphone";
 
-let input = Input();
+
+
 
 
 
@@ -9,19 +10,18 @@ let input = Input();
 @Component({
   selector: 'app-smartphone-list-item',
   standalone: true,
-  imports: [
-    NgIf,
-    NgForOf,
-    NgClass
-  ],
+
   templateUrl: './smartphone-list-item.component.html',
   styleUrl: './smartphone-list-item.component.css'
 })
-export class SmartphoneListItemComponent {
-  /**
-   * Input marks the property as an input property
-   * that can receive data from a parent
-   * ? says it is optional
-   */
-  @Input() item: any;
+export class SmartphoneListItemComponent implements OnInit {
+  @Input() smartPhone!: smartPhone;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
 }
+
