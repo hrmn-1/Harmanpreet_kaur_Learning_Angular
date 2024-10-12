4bb92ad891dca4cc73ff92eea04222749e92cc64
hrmn-1 <w0858805@myscc.ca>
2024-10-12 18:21:42 -0400

    define the list of routes in main.ts and set up the correctly providerRoute

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from
+    '@angular/router';
+import {SmartphoneListComponent } from './app/smartphone-list/smartphone-list.component';
+import {SmartphoneListItemComponent } from './app/smartphone-list-item/smartphone-list-item.component';
 
+
+
+const routes: Routes = [
+  { path: 'smartphone', component: SmartphoneListComponent },
+  { path: 'smartphone/:Model', component: SmartphoneListComponent
+  }
+];
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
+
